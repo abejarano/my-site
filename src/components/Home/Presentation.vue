7536b611866a30ad64f6ecf1a69c8923cc5dac91
angel <angel@alphacode.com.br-at-503688510464>
2020-04-17 22:57:19 -0300

    component presentation

---
+++ src/components/Home/Presentation.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container class="container--fluid pl-0 pr-0 pt-0" style="position: absolute; top: 0; z-index: 1">
+        <v-row>
+            <v-col cols = "4"></v-col>
+            <v-col cols = "8" class="pt-0">
+                <img :src="require('../../assets/Grupo1-min.png')"  contain height="74%" />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component({
+    name: 'Presentation',
+  })
+  export default class Presentation extends Vue {
+
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Presentation/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Presentation from '@/components/Home/Presentation.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Presentation
   }
 }
 </script>
